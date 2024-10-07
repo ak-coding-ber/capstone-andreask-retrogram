848116c7a8ce57b2d8fc27c308948d40235c7377
ak-coding-ber <a.kupka1@gmx.net>
2024-10-07 12:08:03 +0200

    added mongodb adapter - user session data stored in db now

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,6 +1,8 @@
+import { MongoDBAdapter } from "@auth/mongodb-adapter";
 import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github";
 import GoogleProvider from "next-auth/providers/google";
+import clientPromise from "@/db/mongodb";
 
 export const authOptions = {
   providers: [
@@ -13,6 +15,21 @@ export const authOptions = {
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
   ],
+  adapter: MongoDBAdapter(clientPromise),
+  debug: true,
+  callbacks: {
+    async session({ session, user }) {
+      // The user object from the database contains the ID of the user in your database
+
+      session.user.userId = user.id;
+
+      // With the code above you can add the user ID to the session object and use it in your pages
+
+      // Make sure you console.log the session and user objects to see what they contain
+
+      return session;
+    },
+  },
 };
 
 export default NextAuth(authOptions);
